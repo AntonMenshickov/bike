059ea5ff70e56e3f9a9448b874a30b2f2795a184
lesh <fijin@mail.ru>
2016-03-18 16:55:49 +0600

    smth happend

--- desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
+++ desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.noname.simplegame.Sgame;
 
 public class DesktopLauncher {
+    //OMG WTF THIS IS SHIT COMM!!
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         new LwjglApplication(new Sgame(), config);
