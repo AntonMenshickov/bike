e992a70c0aa2d3ca8e65ca5f1eac798f53a8863c
lesh <fijin@mail.ru>
2016-03-18 17:09:03 +0600

    smth happend2

--- desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
+++ desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
@@ -6,7 +6,7 @@ import com.noname.simplegame.Sgame;
 
 public class DesktopLauncher {
     //OMG WTF THIS IS SHIT COMM!!
-    //this dont seem to be a work
+    //this dont seem to be a work2
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         new LwjglApplication(new Sgame(), config);
