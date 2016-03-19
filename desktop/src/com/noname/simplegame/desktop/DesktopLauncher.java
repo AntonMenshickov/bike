37f64d440937c0985b924f972a4946b86b098ec1
Антон <antonmenshickov@gmail.com>
2016-03-20 01:21:11 +0600

    1

--- desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
+++ desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
@@ -5,8 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.noname.simplegame.Sgame;
 
 public class DesktopLauncher {
-    //OMG WTF THIS IS SHIT COMM!!
-    //this dont seem to be a work2
+
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         new LwjglApplication(new Sgame(), config);
