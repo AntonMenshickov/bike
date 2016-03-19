77729c3b5a0e09d94371037ceb0435bdeb1fa7b3
Антон <antonmenshickov@gmail.com>
2016-03-20 01:23:21 +0600

    1

--- core/src/com/noname/simplegame/screens/MainMenuScreen.java
+++ core/src/com/noname/simplegame/screens/MainMenuScreen.java
@@ -2,12 +2,14 @@ package com.noname.simplegame.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
@@ -18,88 +20,51 @@ import com.noname.simplegame.model.AssetLoader;
 /**
  * Created by anton on 14.03.2016.
  */
-public class MainMenuScreen implements Screen, InputProcessor {
+public class MainMenuScreen implements Screen {
     private final Game game;
     private Stage stage;
     private Table table;
-    private TextButton actor;
+    private TextButton play;
     private TextButton exit;
+    private Image bike;
 
     public MainMenuScreen(final Game game) {
         this.game = game;
         this.game.setScreen(this);
-
         AssetLoader.loadButtons();
         stage = new Stage();
         table = new Table();
         table.setFillParent(true);
         Gdx.input.setInputProcessor(stage);
-        table.setFillParent(true);
         TextButtonStyle tbs = new TextButtonStyle();
         tbs.font = new BitmapFont();
         tbs.up = new TextureRegionDrawable(AssetLoader.buttonUp);
         tbs.down = new TextureRegionDrawable(AssetLoader.buttonDown);
-        actor = new TextButton("Play", tbs);
-        actor.addListener(new ChangeListener() {
+        play = new TextButton("Play", tbs);
+        play.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeListener.ChangeEvent event, Actor actor) {
                 new GameScreen(game);
             }
         });
+        table.center();
+        table.setDebug(true);
         exit = new TextButton("exit", tbs);
-        exit.setHeight(30f);
+        exit.setWidth(30f);
         stage.addActor(table);
-        table.add(actor).expand().top().left();
-        table.add(exit).expand().top().right();
+        table.add(play).expand(true,false).top().left();
+        table.add().width(100f);
+        table.add(exit).expand(true,false).top().right();
         table.row();
+        AssetLoader.load();
+        Image bike = new Image(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("data/bikePrew.png")))));
+        table.add().width(100f);
+        table.add(bike).expand(true,true).center();
 
-
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
     }
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -113,7 +78,6 @@ public class MainMenuScreen implements Screen, InputProcessor {
     @Override
     public void resize(int width, int height) {
 
-        //stage.setViewport(new ExtendViewport(640, 480, 800, 480));
     }
 
     @Override
