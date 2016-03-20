087fbc3356b62c70bb8349bd2ea000dd931ac109
Антон <antonmenshickov@gmail.com>
2016-03-20 13:40:32 +0600

    texture packer

--- desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
+++ desktop/src/com/noname/simplegame/desktop/DesktopLauncher.java
@@ -3,11 +3,11 @@ package com.noname.simplegame.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.noname.simplegame.Sgame;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class DesktopLauncher {
-
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new Sgame(), config);
-    }
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sgame(), config);
+	}
 }
